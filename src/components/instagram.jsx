7ecd53698900796b65239c7f52a9b257d694b775
dhanush <therealdhanush@gmail.com>
2025-07-13 16:24:20 +0530

    media changes

--- src/components/instagram.jsx
+++ src/components/instagram.jsx
@@ -11,15 +11,15 @@ import "swiper/css/navigation";
 export default function Instagram() {
     return(
         <section className='mt-10'> 
-        <div className='max-w-6xl mx-auto px-6 py-12 text-center'>
-            <h2 className='text-4xl text-orange-500  font-bold'>Learn with short and powerful Reels</h2>
-            <p className='text-gray-700 mt-4 text-lg mb-12'> Follow our founder on Instagram for bite-sized educational content, exam tips, and learning strategies that make a real impact.</p>
+        <div className='max-w-5xl mx-auto px-6 py-12 text-center'>
+            <h2 className='text-4xl text-orange-500  font-bold'>Learn with short and powerful Videos</h2>
+            <p className='text-gray-700 mt-4 text-lg mb-12 '> Follow our founder on Instagram and Youtube for bite-sized educational content, exam tips, and learning strategies that make a real impact.</p>
                 <Swiper
                     modules={[Autoplay]}
                     spaceBetween={20}
                     loop
                     breakpoints={{
-                        0: { slidesPerView: 2 },
+                        0: { slidesPerView: 1 },
                         640: { slidesPerView: 2 },
                         768: { slidesPerView: 2 },
                         1024: { slidesPerView: 3 },
